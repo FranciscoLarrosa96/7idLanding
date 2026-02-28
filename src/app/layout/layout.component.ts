897ea25fc5a2d1714161b39ca1042bc8ec53547f
Francisco Larrosa <franciscolarrosa22@gmail.com>
2026-02-27 22:20:04 -0300

    refactor: update header links to use 'Servicios' and remove transition animation logic

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,5 @@
-import { Component, OnInit, signal } from '@angular/core';
-import {
-  RouterOutlet,
-  Router,
-  NavigationStart,
-  NavigationEnd,
-} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import * as AOS from 'aos';
@@ -15,20 +10,11 @@ import * as AOS from 'aos';
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent implements OnInit {
-  showTransition = signal(false);
-
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
-      if (event instanceof NavigationStart) {
-        this.showTransition.set(true);
-        // Scroll to top on navigation
-        window.scrollTo({ top: 0, behavior: 'instant' });
-      }
       if (event instanceof NavigationEnd) {
-        setTimeout(() => {
-          this.showTransition.set(false);
-          AOS.refresh();
-        }, 400);
+        window.scrollTo({ top: 0, behavior: 'instant' });
+        AOS.refresh();
       }
     });
   }
